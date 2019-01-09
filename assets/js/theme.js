3fe0a565bcd6fe9722941d75419fb4065ca5d353
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-09 17:21:29 +0300

    2019-01-09 14:21:29

---
+++ assets/js/theme.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Loading functions.
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+$(function () {
+	extJS_getRepoData(metainfo, '9e24a1926a87971c3cd7', 'b203305300eb04727e32514dc5bedb03e35629c8');
+	extJS_setRepoData(metainfo);
+});
